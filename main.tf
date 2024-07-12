7cf36851b28ae75ed4d2135179b19028aeec9c6c
Justin Loi <justin.loi@cms.hhs.gov>
2024-07-11 14:21:19 -1000

    revise iam_policy_document

--- main.tf
+++ main.tf
@@ -135,21 +135,20 @@ resource "aws_security_group" "mssql" {
 
 # IAM Role/Policy Creation for MSSQL Backup
 data "aws_iam_policy_document" "this" {
+  statement{
+      effect  = "Allow"
+      actions = ["sts:AssumeRole"]
 
-  content {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
+      principals {
+        type        = "Service"
+        identifiers = ["rds.amazonaws.com"]
+      }
 
-    principals {
-      type        = "Service"
-      identifiers = ["rds.amazonaws.com"]
-    }
-
-    condition {
-      test     = var.assume_role_condition_test
-      variable = "aws:SourceAccount"
-      values   = [var.aws_id]
-    }
+      condition {
+        test     = var.assume_role_condition_test
+        variable = "aws:SourceAccount"
+        values   = [var.aws_id]
+      }
   }
 }
 
